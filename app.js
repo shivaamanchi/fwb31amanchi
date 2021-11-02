31c799dc94a30c069abac13ede55fe59af47b7b5
shivaamanchi <S545603@NWMISSOURI.EDU>
2021-11-02 18:12:01 -0500

    A good message

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var tomatoRouter = require('./routes/tomato');
+var addmodsRouter = require('./routes/addmods');
 
 var app = express();
 
@@ -21,9 +23,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tomato',tomatoRouter);
+app.use('/addmods',addmodsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(err, req, res, next) {
   next(createError(404));
 });
 
