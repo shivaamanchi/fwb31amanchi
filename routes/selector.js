eca63944886294abb986ec5403485efc18b99c7f
shivaamanchi <S545603@NWMISSOURI.EDU>
2021-11-02 18:38:40 -0500

    A good message

--- routes/selector.js
+++ routes/selector.js
@@ -3,10 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  let query = req.query
-  console.log(`rows ${query.rows}`)
-  console.log(`cols ${query.cols}`)
-  res.render('addmods', { title: 'AddMods', query: query });
+  res.render('selector', { title: 'Selector' });
 });
 
 module.exports = router;
